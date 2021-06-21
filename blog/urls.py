132dd68cb26c43d9e687ca4cd5851b29393eaa7c
Yuriy <to.yuriy@gmail.com>
2021-06-21 14:07:38 +0300

    done

--- blog/urls.py
+++ blog/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path(r'', views.post_list, name='post_list'),
     path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     path(r'^post/new/$', views.post_new, name='post_new'),
+    path(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
