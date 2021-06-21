5f422a23678176d2a0356a11fef806f2519a7108
Yuriy <to.yuriy@gmail.com>
2021-06-21 12:58:02 +0300

    upd1

--- blog/urls.py
+++ blog/urls.py
@@ -12,4 +12,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.post_list, name='post_list'),
+    path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,19 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
-    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     # Post.objects.all()
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
